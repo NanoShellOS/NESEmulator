55ffa0272ec5213765e42db6c86e2ba30d447162
iProgramInCpp <iprogramincpp@gmail.com>
2025-01-31 22:49:33 +0200

    * PPU: Fix some render pipeline issues

--- src/ppu.c
+++ src/ppu.c
@@ -205,6 +205,9 @@ void execute_ppu(PPU* ppu){
 
             if(ppu->mask & SHOW_BG){
                 palette_addr = render_background(ppu);
+			}
+			// note: render pipeline still going if SHOW_BG is zero, if SHOW_SPRITE is 1
+			if (ppu->mask & RENDER_ENABLED) {
                 if(fine_x == 7) {
                     if ((ppu->v & COARSE_X) == 31) {
                         ppu->v &= ~COARSE_X;
@@ -224,7 +227,7 @@ void execute_ppu(PPU* ppu){
             palette_addr = ppu->palette[palette_addr];
             ppu->screen[ppu->scanlines * VISIBLE_DOTS + ppu->dots - 1] = nes_palette[palette_addr];
         }
-        if(ppu->dots == VISIBLE_DOTS + 1 && ppu->mask & SHOW_BG){
+        if(ppu->dots == VISIBLE_DOTS + 1 && ppu->mask & RENDER_ENABLED){
             if((ppu->v & FINE_Y) != FINE_Y) {
                 // increment coarse x
                 ppu->v += 0x1000;
@@ -252,7 +255,7 @@ void execute_ppu(PPU* ppu){
             ppu->v &= ~HORIZONTAL_BITS;
             ppu->v |= ppu->t & HORIZONTAL_BITS;
         }
-        else if(ppu->dots == VISIBLE_DOTS + 4 && ppu->mask & SHOW_SPRITE && ppu->mask & SHOW_BG) {
+        else if(ppu->dots == VISIBLE_DOTS + 4 && ppu->mask & RENDER_ENABLED) {
             ppu->mapper->on_scanline(ppu->mapper);
         }
         else if(ppu->dots == END_DOT && ppu->mask & RENDER_ENABLED){
